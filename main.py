8b1a2a9fb0340ef949e4cb46acb42504d4c64a81
ACBob <ethbot@hotmail.com>
2019-12-06 21:48:16 +0000

    Fixed Collision and made a broken clock function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 class TileEntity(object):
 
@@ -9,7 +10,7 @@ class TileEntity(object):
 
         self.Gravity = gravity
         self.GravitySteps = 120 #Delay between gravity simulations
-        self.NextGravityGametime = GameTime+self.GravitySteps
+        self.NextGravityGametime = GameTime.value+self.GravitySteps
 
         self.Collision = collisions
         self.CollisionBounds = (0,32) #Var 1 = Top left on both axis, Var 2 = Bottom Right on both Axis.
@@ -19,9 +20,9 @@ class TileEntity(object):
         self.turt.speed("fastest")
 
     def GravitySimulation(self):
-        if self.Gravity and self.NextGravityGametime <= GameTime:
+        if self.Gravity and self.NextGravityGametime <= GameTime.value:
             self.Down()
-            self.NextGravityGametime = GameTime+self.GravitySteps
+            self.NextGravityGametime = GameTime.value+self.GravitySteps
 
     def GetCollide(self):
         return ((self.position[0]+self.CollisionBounds[0],self.position[1]+self.CollisionBounds[0]),(self.position[0]+self.CollisionBounds[1],self.position[1]+self.CollisionBounds[1])) #For testing collisions!
@@ -66,7 +67,7 @@ class TileEntity(object):
         return False
 
     def Movement(self,direction): #Direction is an integer, 0 = "UP" 1 + "DOWN" 2 = "LEFT" 3 = "RIGHT"
-        
+        #print(direction)
         if direction < 2:
             self.position = (self.position[0],self.position[1]+((32,-32)[direction]))
             if self.TestCollision():
@@ -74,7 +75,7 @@ class TileEntity(object):
         else:
             self.position = (self.position[0]+((-32,32)[direction-2]),self.position[1])
             if self.TestCollision():
-                self.position = (self.position[0],self.position[1]+((32,-32)[direction]))
+                self.position = (self.position[0],self.position[1]+((32,-32)[direction-2]))
         
 
 class Player(TileEntity):
@@ -89,7 +90,27 @@ class Player(TileEntity):
 
         self.turt.color("Red","Pink")
 
+class Clock(object):
 
+    def __init__(self,startnum=0):
+        print("Clock {} initialised.".format(self))
+        self.value = 0
+        self.LastCall = time.time()
+        self.Calls = [0]
+
+    def tick(self,amount=1):
+        self.Calls = self.Calls[-10:]
+        self.Calls.append(time.time()-self.LastCall)
+        #print(self.Calls)
+        self.value += 1
+
+    def GetAverageCalls(self,indice=10):
+        avg = 0
+        for i in self.Calls[indice:]:
+            avg+=i
+        if avg > 0 and len(self.Calls) > 0:
+            avg = avg/indice
+        return avg
 
 global Screen
 Screen = turtle.Screen()
@@ -97,7 +118,7 @@ Screen.delay(0)
 Screen.bgcolor("Black")
 
 global GameTime #Turtle doesn't have the luxury of built-in time knowledge.
-GameTime = 0
+GameTime = Clock()
 
 global RenderList
 RenderList = []
@@ -112,10 +133,13 @@ Screen.listen()
 
 #You can comment out "turtle.tracer" and "turtle.update" if you want some screen flicker.
 
+#9223372036854775807
+
 while True:
     turtle.tracer(0,0) #Hide Changes
     for i in RenderList: i.render()
     player.render()
     turtle.update() #Push Changes
-    GameTime+=1
-    Screen.title(GameTime)
+    GameTime.tick()
+    
+    Screen.title(GameTime.GetAverageCalls(10))
